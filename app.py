d9fc1904b0100548cc74d13e648907f481739ced
breannabisnott <breanna.bisnott@gmail.com>
2024-02-25 02:49:33 -0500

    to be continued

--- app.py
+++ app.py
@@ -12,7 +12,19 @@ config = dotenv_values(".env")
 client = motor.motor_asyncio.AsyncIOMotorClient(config["MONGO_URL"])
 db = client.tank_data
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [ "https://ecse3038-lab3-tester.netlify.app" ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
@@ -22,6 +34,28 @@ class Tank(BaseModel):
     lat: Optional[float] = None
     long: Optional[float] = None
 
+class Profile(BaseModel):
+    last_updated: Optional[str] = None
+    username: Optional[str] = None
+    role: Optional[str] = None
+    color: Optional[str] = None
+
+@app.get("/profile")
+async def get_profile():
+    profile = await db["profiles4"].find().to_list(1)
+    return TypeAdapter(List[Profile]).validate_python(profile)
+
+@app.post("/profile", status_code=201)
+async def create_profile(profile: Profile):
+        
+    #insert some if here
+        new_profile = await db["profiles4"].insert_one(profile.model_dump())
+
+        created_profile = await db["profiles4"].find_one({"_id": new_profile.inserted_id})
+        return Profile(**created_profile)
+    #raise HTTPException(status_code = 400, detail = "Unable to create more than 1 profile")
+
+
 @app.get("/tank")
 async def get_tanks():
     tanks = await db["tanks4"].find().to_list(999)
@@ -29,8 +63,10 @@ async def get_tanks():
 
 @app.get("/tank/{id}")
 async def get_tank(id: str):
-    tank = await db["tanks4"].find_one({"_id": ObjectId(id)})
-    return Tank(**tank)
+    #insert if
+        tank = await db["tanks4"].find_one({"_id": ObjectId(id)})
+        return Tank(**tank)
+    #raise HTTPException(status_code = 404, detail = "Tank of id: " + id + " not found.")
 
 @app.post("/tank", status_code=201)
 async def create_tank(tank: Tank):
